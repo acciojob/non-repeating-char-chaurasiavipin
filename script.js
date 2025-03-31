6ff0d507c7d31b1ba7f0a0bbc30a512346c899a5
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-03-31 19:12:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ //
 	let map=new Map()
-  for(let char of str){
-    if(map.has(char)){
-      map.set(char,map.get(char)+1)
-    }
-    else{
-      map.set(char,1)
-    }
-  }
-  let occurence=Infinity
-  let num=str[0]
-  map.forEach(function(value,key){
-    if(value<occurence){
-        occurence=value
-        num=key
-        //console.log(value)
-      }
-      // else if(value===occurence){
-      //   if(num<key)
-      // key=num
-      // }
-      else if (value === occurence) {
-      if (num > key) {
-    num = key;  // Update num if key is smaller
+	for(let a of str ){
+		if(map.has(a)){
+			map.set(a,map.get(a)+1)
 }
-        // num = num < key ? num : key;  // Choose the smaller character
-    }
-  })
-  return num
+		else{
+			map.set(a,1)
+			
+			
+		}
 }
+	let ans=null;
+	map.forEach((value,key)=>{
+       // console.log(key+"=>"+typeof(value))
+		if(value==1){
+ans=key
+return ans
+		}
+		
+	})
+	return ans
+	
+	 //aab
+	//aaaaaa
+	//aabb
+	//abc
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
